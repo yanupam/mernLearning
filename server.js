55730e58a2979f07197b3752be31df32b9d478ff
yanupam <yanupam@outlook.com>
2024-05-07 00:40:05 +0530

    middleWare added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express =require('express')
 const app = express();
 app.use(express.json());
-
+//mongodb+srv://anupam:lucknow01@anupam.2pe6v1t.mongodb.net/
 let dataStore = [
     {   
         id:1,
@@ -14,7 +14,24 @@ let dataStore = [
         age:22
     }
 ];
-app.get('/get',(req,res)=>{
+
+const authMiddleWare = (req,res,next)=>{
+   try {
+    const username = req.headers.username;
+    console.log('AUTH');
+     if(username !=='Anupam'){
+    console.log('AUTHDone');
+        return res.status(401).json({ error: 'Unauthorized' });
+     }else{
+         next();
+     } 
+   } catch (error) {
+    res.status(401).json({ error: 'Invalid Token' });
+   }
+}
+
+
+app.get('/get',authMiddleWare,(req,res)=>{
     const id = req.query.id;
     for(let listData of dataStore){
         if(listData.id == id){return  res.status(200).send(listData);} 
